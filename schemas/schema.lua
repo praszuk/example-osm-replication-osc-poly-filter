a7fc1c81beff51ec7215752062b161082cd05b95
Paweł Raszuk <pawel.raszuk@gmail.com>
2026-04-21 01:31:35 +0200

    Add example files

---
+++ schemas/schema.lua
@@ -0,0 +1,91 @@
+-- https://github.com/osm2pgsql-dev/osm2pgsql/tree/master/flex-config#flex-output-configuration
+local tables = {}
+
+tables.nodes = osm2pgsql.define_node_table('nodes', {
+    { column = 'version', type = 'int' },
+    { column = 'timestamp', sql_type = 'timestamptz' },
+    { column = 'changeset', type = 'bigint' },
+    { column = 'uid', type = 'bigint' },
+    { column = 'username', type = 'text' },
+    { column = 'tags', type = 'jsonb' },
+    { column = 'geom', type = 'point', projection = 4326 }
+}, { indexes = {
+        { column = 'tags', method = 'gin' },
+        { column = 'geom', method = 'gist' },
+        { column = 'timestamp', method = 'btree' },
+    }
+})
+
+tables.ways = osm2pgsql.define_way_table('ways', {
+    { column = 'version', type = 'int' },
+    { column = 'timestamp', sql_type = 'timestamptz' },
+    { column = 'changeset', type = 'bigint' },
+    { column = 'uid', type = 'bigint' },
+    { column = 'username', type = 'text' },
+    { column = 'tags', type = 'jsonb' },
+    { column = 'geom', type = 'linestring', projection = 4326 },
+}, { indexes = {
+        { column = 'tags', method = 'gin' },
+        { column = 'geom', method = 'gist' },
+        { column = 'timestamp', method = 'btree' },
+    }
+})
+
+tables.relations = osm2pgsql.define_relation_table('relations', {
+    { column = 'version', type = 'int' },
+    { column = 'timestamp', sql_type = 'timestamptz' },
+    { column = 'changeset', type = 'bigint' },
+    { column = 'uid', type = 'bigint' },
+    { column = 'username', type = 'text' },
+    { column = 'tags', type = 'jsonb' },
+    { column = 'members', type = 'jsonb' },
+    { column = 'geom', type = 'multipolygon', projection = 4326 },
+}, { indexes = {
+        { column = 'tags', method = 'gin' },
+        { column = 'geom', method = 'gist' },
+        { column = 'timestamp', method = 'btree' },
+    }
+})
+
+local function format_dt(ts)
+    return os.date('!%Y-%m-%dT%H:%M:%SZ', ts)
+end
+
+function osm2pgsql.process_node(object)
+    tables.nodes:insert({
+        version = object.version,
+        timestamp = format_dt(object.timestamp),
+        changeset = object.changeset,
+        uid = object.uid,
+        username = object.user,
+        tags = object.tags,
+        geom = object:as_point()
+    })
+end
+
+
+function osm2pgsql.process_way(object)
+    tables.ways:insert({
+        version = object.version,
+        timestamp = format_dt(object.timestamp),
+        changeset = object.changeset,
+        uid = object.uid,
+        username = object.user,
+        tags = object.tags,
+        geom = object:as_linestring()
+    })
+end
+
+
+function osm2pgsql.process_relation(object)
+    tables.relations:insert({
+        version = object.version,
+        timestamp = format_dt(object.timestamp),
+        changeset = object.changeset,
+        uid = object.uid,
+        username = object.user,
+        tags = object.tags,
+        members = object.members,
+        geom = object:as_multipolygon()
+    })
+end
